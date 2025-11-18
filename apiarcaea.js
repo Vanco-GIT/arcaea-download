f08c284cc49a4b4fd22888c13e121b260995d132
Vanco_CN <Vanco_Personal@163.com>
2025-11-18 23:18:38 +0800

    Delete apiarcaea.js

--- apiarcaea.js
+++
@@ -1,7 +0,0 @@
-// 保存为 api/arcaea.js
-export default async (req, res) => {
-  const up = await fetch('https://webapi.lowiro.com/webapi/serve/static/bin/arcaea/apk');
-  const body = await up.json();
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json(body);
-};
